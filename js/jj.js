a5597e55eb634d6fc866549a12d22648cd4c5041
Sean Pheneger <wcolts2000@gmail.com>
2018-07-31 00:55:30 -0700

    initial commit

---
+++ js/jj.js
@@ -0,0 +1,24 @@
+window.onscroll = function () {
+  stickyHeader()
+};
+
+let header = document.getElementById("myHeader");
+
+let sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+function hamburgerMenuToggle() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
