d7788bce4f09ce13c14067d6c0db77f7872d5251
Sean Pheneger <wcolts2000@gmail.com>
2018-08-01 01:04:57 -0700

    added smoothscroll, jquery, some additoinal media queries, styled shop cats

--- js/jj.js
+++ js/jj.js
@@ -22,3 +22,33 @@ function hamburgerMenuToggle() {
     x.style.display = "block";
   }
 }
+
+function elmYPosition(eID) {
+  var elm = document.getElementById(eID);
+  var y = elm.offsetTop;
+  var node = elm;
+  while (node.offsetParent && node.offsetParent != document.body) {
+    node = node.offsetParent;
+    y += node.offsetTop;
+  }
+  return y;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("up-arrow").style.display = "block";
+  } else {
+    document.getElementById("up-arrow").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
